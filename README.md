93d326949a9eaf7432fe10b30b76b84295da4ac0
nextgtrgod <KYakovlev7@gmail.com>
2018-11-14 23:28:20 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # portfolio-page
+### Frontend-developer portfolio page
+
+* [Express.js](http://expressjs.com/)
+* [Pug](https://pugjs.org/)
+* [vanilla-tilt.js](https://github.com/micku7zu/vanilla-tilt.js/)
+
+
+![screen](https://image.ibb.co/cs8DDR/portfolio_page_1.jpg)
+![screen-inverse-theme](https://image.ibb.co/nLOftR/portfolio_page_2.jpg)
+
+[DEMO](http://nextgtrgod.info/)
+
+
+### Todo
+- [x] page scroll animation
+- [x] inverse theme
+- [x] invert trigger more clickable
+- [x] projects section content
+- [x] about section content
+- [x] contacts
+- [x] scroll to button
+- [x] mobile-friendly (less animations)
---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+<div id="app">
+	<div class="scroll-snap"/>
+	<div class="scroll-snap"/>
+	<div class="scroll-snap"/>
+
+	<main>
+
+	</main>
+</div>
+</template>
+
+
+<script>
+import Events from '@/utils/events'
+import checkBrowser from '@/utils/checkBrowser'
+
+export default {
+	name: 'App',
+	data() {
+		return {
+
+		}
+	},
+	beforeCreate() {
+		checkBrowser()
+	},
+	created() {
+
+	},
+	mounted() {
+
+	},
+}
+</script>
+
+
+<style lang="less">
+@import "./styles/normalize.less";
+@import "./styles/variables.less";
+@import "./styles/animations.less";
+@import "./styles/scrollbar.less";
+@import "./styles/cursor.less";
+
+
+html, body {
+	font-family: @font-default;
+	background-color: @color-bg;
+}
+
+body {
+	overflow: hidden;
+}
+
+
+#app {
+	scroll-snap-type: y mandatory;
+	overflow-y: scroll;
+	max-height: 100vh;
+	opacity: 0;
+	animation: fade-in .4s forwards;
+}
+
+.scroll-snap {
+	height: 100vh;
+	scroll-snap-align: start;
+	scroll-snap-stop: always;
+}
+
+
+main {
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+}
+
+</style>
